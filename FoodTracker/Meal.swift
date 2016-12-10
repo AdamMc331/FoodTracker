5fd88feef44bd6a9a58892d4198af7acf6e55050
AdamMc331 <amcneilly331@gmail.com>
2016-12-10 17:10:20 -0500

    Added model and unit testing.

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,27 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Adam McNeilly on 12/10/16.
+//  Copyright © 2016 Adam McNeilly. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -11,26 +11,19 @@ import XCTest
 
 class FoodTrackerTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // MARK: FoodTracker Tests
+    // Tests to confirm that the Meal initializer returns when no name or a negative rating is provided.
+    func testMealInitialization() {
+        // Success case
+        let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
+        
+        // Bad name
+        let noName = Meal(name: "", photo: nil, rating: 5)
+        XCTAssertNil(noName)
+        
+        // Bad rating
+        let badRating = Meal(name: "Newest meal", photo: nil, rating: -1)
+        XCTAssertNil(badRating)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
